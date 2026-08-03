72d4203589c4b8b76c1ac72fedd7261f35b8fc0e
Shukrullo <abdusalomovjovox@gmail.com>
2026-08-03 10:04:21 +0500

    news5

--- client/src/components/News/NewsCardSkeleton.jsx
+++ client/src/components/News/NewsCardSkeleton.jsx
@@ -7,7 +7,9 @@ const NewsCardSkeleton = () => {
   return (
     <article className="news-card news-card--skeleton" aria-hidden>
       <div className="news-card-media">
-        <LoaderSkeleton variant="image" className="news-card-skel-media" />
+        <div className="news-card-skel-media">
+          <LoaderSkeleton variant="block" />
+        </div>
       </div>
 
       <div className="news-card-body">
@@ -15,49 +17,22 @@ const NewsCardSkeleton = () => {
           variant="text"
           className="news-card-skel-name"
           width="88%"
-          height={22}
         />
-        <LoaderSkeleton
-          variant="text"
-          className="news-card-skel-description"
-          width="100%"
-          height={14}
-        />
-        <LoaderSkeleton
-          variant="text"
-          className="news-card-skel-description"
-          width="96%"
-          height={14}
-        />
-        <LoaderSkeleton
-          variant="text"
-          className="news-card-skel-description"
-          width="90%"
-          height={14}
-        />
-        <LoaderSkeleton
-          variant="text"
-          className="news-card-skel-description"
-          width="84%"
-          height={14}
-        />
-        <LoaderSkeleton
-          variant="text"
-          className="news-card-skel-description news-card-skel-description--last"
-          width="72%"
-          height={14}
-        />
+        <div className="news-card-skel-description-wrap">
+          <LoaderSkeleton variant="text" className="news-card-skel-description" width="100%" />
+          <LoaderSkeleton variant="text" className="news-card-skel-description" width="94%" />
+          <LoaderSkeleton variant="text" className="news-card-skel-description" width="86%" />
+        </div>
 
         <div className="news-card-date news-card-skel-date">
-          <LoaderSkeleton variant="text" width={88} height={14} />
-          <LoaderSkeleton variant="text" width={54} height={14} />
+          <LoaderSkeleton variant="text" width={88} />
+          <LoaderSkeleton variant="text" width={54} />
         </div>
 
         <LoaderSkeleton
-          variant="button"
+          variant="text"
           className="news-card-skel-more"
           width={120}
-          height={18}
         />
       </div>
     </article>
